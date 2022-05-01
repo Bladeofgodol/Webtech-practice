b48009865f6d2458180f696f24bda67f6bbb5fbd
Blade <kebededan0@gmail.com>
2022-05-01 14:58:21 +0300

    first commit

---
+++ 4.Python/userinput.py
@@ -0,0 +1,3 @@
+name = input('please name: ') #to input by users 
+print('yourname is: \n' + name) #use + for concationation or adding the input value & \n for new line
+print(f'yourname is: {name}') #f string lets u format ur datatype
